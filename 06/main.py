83b9bb00f98d1b33ef4fb9d5a1855ef55cbc06f4
Kavuti <christian.cavuti@gmail.com>
2023-12-06 12:22:31 +0100

    completed day 6

---
+++ 06/main.py
@@ -0,0 +1,42 @@
+import os
+import math
+
+
+def read_input():
+    with open(os.getcwd() + "/input.txt", "r") as f:
+        lines = f.read().splitlines()
+        times = [int(n) for n in lines[0].split(":")[1].split(" ") if n != ""]
+        distances = [int(n) for n in lines[1].split(":")[1].split(" ") if n != ""]
+    return times, distances
+
+
+def read_input2():
+    with open(os.getcwd() + "/input.txt", "r") as f:
+        lines = f.read().splitlines()
+        times = int(lines[0].split(":")[1].replace(" ", ""))
+        distances = int(lines[1].split(":")[1].replace(" ", ""))
+    return times, distances
+
+
+def part1():
+    times, distances = read_input()
+    winning = [0 for _ in times]
+    for race, time in enumerate(times):
+        top = math.floor(((time) + math.sqrt(time**2 - 4 * distances[race])) / 2)
+        bot = math.ceil(((time) - math.sqrt(time**2 - 4 * distances[race])) / 2)
+
+        winning[race] = top - bot + 1
+    print(math.prod(winning))
+
+
+def part2():
+    time, distance = read_input2()
+    top = math.floor(((time) + math.sqrt(time**2 - 4 * distance)) / 2)
+    bot = math.ceil(((time) - math.sqrt(time**2 - 4 * distance)) / 2)
+
+    print(top - bot + 1)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
